35481798e136324ad3dc9d4582ce44537ef31d1c
Eduardo Castro <eduardocastro.a@hotmail.com>
2022-07-19 18:38:39 -0400

    Add console log

--- area.js
+++ area.js
@@ -1,44 +1,34 @@
-function calculateRectangleArea(length, width) {
+const calculateRectangleArea = function(length, width) {
   let area = 0;
-  
+
   if (length < 0 || width < 0) {
     area = undefined;
-  
   } else area = length * width;
 
-  
-  return area; 
-}
-
+  return area;
+};
 console.log(calculateRectangleArea(2, 5));
 
-function calculateTriangleArea(base, height) {
+const calculateTriangleArea = function(base, height) {
   let area = 0;
-  
+
   if (base < 0 || height < 0) {
     area = undefined;
-  
   } else area = (base * height) / 2;
-
-  
   return area;
-} 
+};
 
 console.log(calculateTriangleArea(-1, 4));
 
-
-function calculateCircleArea(radius) {
+const calculateCircleArea = function(radius) {
   let area = 0;
-  
+
   if (radius < 0) {
     area = undefined;
-  
-  } else 
-    area = Math.pow(radius,2) * Math.PI
-
-  
+  } else area = Math.pow(radius, 2) * Math.PI;
   return area;
-}
+};
 
 console.log(calculateCircleArea(-10));
-console.log('hello');
+console.log("hello ");
+console.log('sudbury')
